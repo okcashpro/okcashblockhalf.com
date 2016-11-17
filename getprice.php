79af09e9c46fd048d5ec5d0069ed4add00724c28
oktoshi <devteam@okcash.co>
2016-11-17 01:26:16 -0600

    cleanup

--- getprice.php
+++
@@ -1,9 +0,0 @@
-<?php
-$data = file_get_contents('https://api.cryptonator.com/api/ticker/ok-usd');
-$price = json_decode($data, true);
-$btcPrice = (float)$price["ticker"]["price"];
-$file = "price.txt";
-$fh = fopen($file, 'w') or die("can't open file");
-fwrite($fh, $btcPrice);
-fclose($fh);
-?>
